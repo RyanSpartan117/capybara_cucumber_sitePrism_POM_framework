54bfb68ae69f18973bf25f5609c74d084ca41a3e
Ryan Chapman <rchapman@spartaglobal.co>
2017-07-13 10:09:34 +0100

    capybara cucumber site prism framework setup

---
+++ features/support/env.rb
@@ -0,0 +1,12 @@
+require_relative '../../lib/helpers/environment_setup.rb'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'site_prism'
+require 'active_support'
+include EnvironmentSetup
+
+ActiveSupport::Dependencies.autoload_paths << File.expand_path(File.join(Dir.pwd, 'lib'))
+
+driver_setup :chrome
+
+capybara_default_setup
