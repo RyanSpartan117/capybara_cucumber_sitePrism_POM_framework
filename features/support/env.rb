6e412c76e09a829b65be9dc53391996e68b392a8
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2019-11-20 16:02:48 +0000

    added rspec matchers and a timeout. Added requires

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,14 @@
 require_relative '../../lib/helpers/environment_setup.rb'
 require 'capybara/cucumber'
-require 'selenium-webdriver'
+require 'rspec'
 require 'site_prism'
 require 'require_all'
 require 'webdrivers'
 require 'os'
+require 'pry'
+
 include EnvironmentSetup
+include RSpec::Matchers
 
 require_rel "../../lib/pages/*.rb"
 require_rel "../../lib/*.rb"
@@ -22,6 +25,7 @@ end
 Capybara.register_driver :custom_chrome_headless do |app|
 	browser_options = ::Selenium::WebDriver::Chrome::Options.new()
 	browser_options.args << '--window-size=1920,1080'
+	browser_options.timeout << 5
 	Capybara::Selenium::Driver.new(app,
 		browser: :chrome,
 		options: browser_options
