492a220d3cab1647fbee7c245ecd7359cd2ed9ba
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2019-11-20 11:42:19 +0000

    added chromedriver support

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,8 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'site_prism'
 require 'require_all'
+require 'webdrivers'
+require 'os'
 include EnvironmentSetup
 
 require_rel "../../lib/pages/*.rb"
@@ -11,3 +13,17 @@ require_rel "../../lib/*.rb"
 driver_setup :chrome
 
 capybara_default_setup
+
+if OS.windows?
+	require 'webdrivers'
+	Webdrivers::Chromedriver.required_version = '78.0.3904.70'
+end
+
+Capybara.register_driver :custom_chrome_headless do |app|
+	browser_options = ::Selenium::WebDriver::Chrome::Options.new()
+	browser_options.args << '--window-size=1920,1080'
+	Capybara::Selenium::Driver.new(app,
+		browser: :chrome,
+		options: browser_options
+	)
+end
