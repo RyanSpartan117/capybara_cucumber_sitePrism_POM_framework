54cc460cdd151bf51f345744059a8d2698afe723
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2019-11-20 16:03:00 +0000

    added assertion, uses capybara finder over siteprism now

--- lib/pages/bbc_home_page.rb
+++ lib/pages/bbc_home_page.rb
@@ -2,5 +2,13 @@ class Bbchomepage < SitePrism::Page
   set_url "http://bbc.co.uk"
   set_url_matcher /bbc.co.uk/
 
-  element :sign_in_link, "span[id='idcta-username']"
+
+  def clickSignInLink
+	sign_in_link = find('#idcta-username')
+	sign_in_link.click
+  end
+
+  def assertHomePage
+  	expect(page.title).to eql('BBC - Home')
+  end
 end
