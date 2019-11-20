da29a61e9e5749ed8944c1c7857da1c3ea7c3f92
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2019-11-20 16:02:25 +0000

    variable name change, added a method for asserting

--- features/steps/bbc_login_steps.rb
+++ features/steps/bbc_login_steps.rb
@@ -1,9 +1,10 @@
 Given(/^I can access the BBC home page$/) do
-  @Bbcwebsite = Bbcwebsite.new
-  @Bbcwebsite.bbc_home_page.load
-  @Bbcwebsite.bbc_home_page.displayed?
+  @bbcwebsite = Bbcwebsite.new
+  @bbcwebsite.bbc_home_page.load
+  @bbcwebsite.bbc_home_page.displayed?
 end
 
 Then(/^The sign in button exists$/) do
-  @Bbcwebsite.bbc_home_page.has_sign_in_link?
+  @bbcwebsite.bbc_home_page.clickSignInLink
+  @bbcwebsite.bbc_home_page.assertHomePage
 end
