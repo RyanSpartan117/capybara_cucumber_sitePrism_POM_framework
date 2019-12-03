b3f17047bd28aaed4d3ace116f0bd8c6e8a56a92
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2019-12-03 14:33:57 +0000

    added a test that needs to be debugged

--- features/automationWorkshop.feature
+++ features/automationWorkshop.feature
@@ -5,3 +5,9 @@ Feature: Click on the big button
         Given I am on the automation workshop page
         When I click on the big button
         Then I can see the big button confirmation text
+
+    @SmallButton
+    Scenario: I can click on the big button
+        Given I am on the automation workshop page
+        When I click on the small button
+        Then I can see the small button confirmation text
--- features/steps/automation_workshop.rb
+++ features/steps/automation_workshop.rb
@@ -1,5 +1,6 @@
 Given("I am on the automation workshop page") do
-
+	visit('https://topupmockserver.azurewebsites.net/automationWorkshop')
+	expect(page.title).to eql('Autonation Workshop Practice')
 end
 
 When("I click on the big button") do
@@ -8,4 +9,14 @@ end
 
 Then("I can see the big button confirmation text") do
     pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+When("I click on the small button") do
+   find('.anotherButton').click
+end
+
+Then("I can see the small button confirmation text") do
+	buttonFeedback = find('#buttonClickFeedback')
+	expect(butonFeedback).to eql('You have clicked the smaller button!')
 end
