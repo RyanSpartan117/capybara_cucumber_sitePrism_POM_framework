6ce8f57a586406f638c62f16bfab4d11e05e6fc9
Ryan Chapman <ryan_chapman3@hotmail.co.uk>
2020-11-24 14:00:41 +0000

    different default driver

--- lib/helpers/environment_setup.rb
+++ lib/helpers/environment_setup.rb
@@ -11,7 +11,7 @@ module EnvironmentSetup
       config.run_server = false #To ensure a Rack server does not start
       config.ignore_hidden_elements = false #to ensure that all hidden elements on a page are recorded/available
       config.default_max_wait_time= 10 #wait time for asynchronus processes to finsh
-      config.default_driver = :chrome #Default browser - input :selenium_chrome to use chrome browser as stated in the driver registration below
+      config.default_driver = :chrome_headless #Default browser - input :selenium_chrome to use chrome browser as stated in the driver registration below
       config.match = :prefer_exact #this setting is to ensure Capybara has specific matching rather than fuzzy logic
     end
   end
